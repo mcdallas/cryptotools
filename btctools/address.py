149dd07d73bd3fcbe235715f15f83b933d256646
mcdallas <mc-dallas@hotmail.com>
2018-02-28 14:36:03 +0000

    bech32 address support

--- btctools/address.py
+++ btctools/address.py
@@ -1,27 +1,100 @@
 import hashlib
 from time import time
 from datetime import timedelta
+from functools import partial
 
-from btctools import base58
+from btctools import base58, bech32
 from ECDS.secp256k1 import generate_keypair
-from transformations import bytes_to_hex
-
-"""Use http://gobittest.appspot.com/Address to make sure that you are producing the correct addresses"""
+from transformations import bytes_to_hex, int_to_bytes
 
 
 sha256 = lambda x: hashlib.sha256(x).digest()
 ripemd160 = lambda x: hashlib.new('ripemd160', x).digest()
+hash160 = lambda x: ripemd160(sha256(x))
 
-versions = {'P2PKH': b'\x00', 'P2SH': b'\x05'}
+HRP = 'bc'
 
 
-def pubkey_to_address(pub, version='P2PKH'):
-    version_byte = versions[version.upper()]
-    hashed = ripemd160(sha256(pub))
-    payload = version_byte + hashed
+def legacy_address(pub_or_script, version_byte):
+    """https://en.bitcoin.it/wiki/Technical_background_of_version_1_Bitcoin_addresses"""
+    hashed = hash160(pub_or_script)
+    payload = int_to_bytes(version_byte) + hashed
     checksum = sha256(sha256(payload))[:4]
     address = payload + checksum
     return base58.encode(address)
+
+
+def pubkey_to_p2wpkh(pub, version_byte, witver):
+    """https://github.com/bitcoin/bips/blob/master/bip-0142.mediawiki#specification"""
+    payload = int_to_bytes(version_byte) + int_to_bytes(witver) + b'\x00' + hash160(pub)
+    checksum = sha256(sha256(payload))[:4]
+    return base58.encode(payload + checksum)
+
+
+def script_to_p2wsh(script, version_byte, witver):
+    """https://github.com/bitcoin/bips/blob/master/bip-0142.mediawiki#specification"""
+    payload = int_to_bytes(version_byte) + int_to_bytes(witver) + b'\x00' + sha256(script)
+    checksum = sha256(sha256(payload))[:4]
+    return base58.encode(payload + checksum)
+
+
+def to_bech32(pubkey_or_script, witver):
+    assert isinstance(pubkey_or_script, bytes)
+    is_key = pubkey_or_script.startswith((b'\02', b'\03')) and len(pubkey_or_script) == 33
+    return pubkey_to_bech32(pubkey_or_script, witver) if is_key else script_to_bech32(pubkey_or_script, witver)
+
+
+def script_to_bech32(script, witver):
+    """https://github.com/bitcoin/bips/blob/master/bip-0141.mediawiki#witness-program"""
+    witprog = sha256(script)
+    return bech32.encode(HRP, witver, witprog)
+
+
+def pubkey_to_bech32(pub, witver):
+    """https://github.com/bitcoin/bips/blob/master/bip-0141.mediawiki#witness-program"""
+    witprog = hash160(pub)
+    return bech32.encode(HRP, witver, witprog)
+
+
+versions = {
+    'P2PKH': partial(legacy_address, version_byte=0x00),
+    'P2SH': partial(legacy_address, version_byte=0x05),
+    'P2WPKH': partial(pubkey_to_p2wpkh, version_byte=0x06, witver=0x00),
+    'P2WSH': partial(script_to_p2wsh, version_byte=0x0A, witver=0x00),
+    'BECH32': partial(to_bech32, witver=0x00),
+}
+
+
+def pubkey_to_address(pub, version='P2PKH'):
+    """Input is a public key for P2PKH/P2WPKH and a script for P2SH/P2wSH"""
+    converter = versions[version.upper()]
+    return converter(pub)
+
+
+def op_push(i):
+    """https://en.bitcoin.it/wiki/Script#Constants"""
+    if i < 0x4c:
+        return int_to_bytes(i)
+    elif i < 0xff:
+        return b'\x4c' + int_to_bytes(i)
+    elif i < 0xffff:
+        return b'\x4d' + int_to_bytes(i)
+    else:
+        return b'\x4e' + int_to_bytes(i)
+
+
+def address_to_script(addr):
+    """https://github.com/bitcoin/bips/blob/master/bip-0173.mediawiki#segwit-address-format"""
+    hrp, _ = bech32.bech32_decode(addr)
+    if hrp not in ('bc', 'tb'):
+        raise bech32.Bech32DecodeError('Invalid human-readable part')
+    witver, witprog = bech32.decode(hrp, addr)
+    if not (0 <= witver <= 16):
+        raise bech32.Bech32DecodeError('Invalid witness version')
+
+    OP_n = int_to_bytes(witver + 0x50 if witver > 0 else 0)
+    script = OP_n + op_push(len(witprog)) + bytes(witprog)
+    return script
 
 
 def vanity(prefix):
---
+++ btctools/bech32.py
@@ -0,0 +1,146 @@
+# Copyright (c) 2017 Pieter Wuille
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+"""Reference implementation for Bech32 and segwit addresses - tweaked to provide descriptive errors"""
+
+
+class Bech32DecodeError(Exception):
+    pass
+
+
+CHARSET = "qpzry9x8gf2tvdw0s3jn54khce6mua7l"
+
+
+def bech32_polymod(values):
+    """Internal function that computes the Bech32 checksum."""
+    generator = [0x3b6a57b2, 0x26508e6d, 0x1ea119fa, 0x3d4233dd, 0x2a1462b3]
+    chk = 1
+    for value in values:
+        top = chk >> 25
+        chk = (chk & 0x1ffffff) << 5 ^ value
+        for i in range(5):
+            chk ^= generator[i] if ((top >> i) & 1) else 0
+    return chk
+
+
+def bech32_hrp_expand(hrp):
+    """Expand the HRP into values for checksum computation."""
+    return [ord(x) >> 5 for x in hrp] + [0] + [ord(x) & 31 for x in hrp]
+
+
+def bech32_verify_checksum(hrp, data):
+    """Verify a checksum given HRP and converted data characters."""
+    return bech32_polymod(bech32_hrp_expand(hrp) + data) == 1
+
+
+def bech32_create_checksum(hrp, data):
+    """Compute the checksum values given HRP and data."""
+    values = bech32_hrp_expand(hrp) + data
+    polymod = bech32_polymod(values + [0, 0, 0, 0, 0, 0]) ^ 1
+    return [(polymod >> 5 * (5 - i)) & 31 for i in range(6)]
+
+
+def bech32_encode(hrp, data):
+    """Compute a Bech32 string given HRP and data values."""
+    combined = data + bech32_create_checksum(hrp, data)
+    return hrp + '1' + ''.join([CHARSET[d] for d in combined])
+
+
+def bech32_decode(bech):
+    """Validate a Bech32 string, and determine HRP and data."""
+    if any(ord(x) < 33 or ord(x) > 126 for x in bech):
+        raise Bech32DecodeError('Character outside the US-ASCII [33-126] range')
+
+    if (bech.lower() != bech) and (bech.upper() != bech):
+        raise Bech32DecodeError('Mixed upper and lower case')
+
+    bech = bech.lower()
+    pos = bech.rfind('1')
+
+    if pos == 0:
+        raise Bech32DecodeError('Empty human readable part')
+    elif pos == -1:
+        raise Bech32DecodeError('No seperator character')
+    elif pos + 7 > len(bech):
+        raise Bech32DecodeError('Checksum too short')
+
+    if len(bech) > 90:
+        raise Bech32DecodeError('Max string length exceeded')
+
+    if not all(x in CHARSET for x in bech[pos+1:]):
+        raise Bech32DecodeError('Character not in charset')
+
+    hrp = bech[:pos]
+    data = [CHARSET.find(x) for x in bech[pos+1:]]
+
+    if not bech32_verify_checksum(hrp, data):
+        raise Bech32DecodeError('Invalid checksum')
+
+    return hrp, data[:-6]
+
+
+def convertbits(data, frombits, tobits, pad=True):
+    """General power-of-2 base conversion."""
+    acc = 0
+    bits = 0
+    ret = []
+    maxv = (1 << tobits) - 1
+    max_acc = (1 << (frombits + tobits - 1)) - 1
+    for value in data:
+        if value < 0 or (value >> frombits):
+            return None
+        acc = ((acc << frombits) | value) & max_acc
+        bits += frombits
+        while bits >= tobits:
+            bits -= tobits
+            ret.append((acc >> bits) & maxv)
+    if pad:
+        if bits:
+            ret.append((acc << (tobits - bits)) & maxv)
+    elif bits >= frombits or ((acc << (tobits - bits)) & maxv):
+        return None
+    return ret
+
+
+def decode(hrp, addr):
+    """Decode a segwit address."""
+    hrpgot, data = bech32_decode(addr)
+    if hrpgot != hrp:
+        raise Bech32DecodeError('Human readable part mismatch')
+
+    decoded = convertbits(data[1:], 5, 8, False)
+    if decoded is None or len(decoded) < 2:
+        raise Bech32DecodeError('Witness programm too short')
+    elif len(decoded) > 40:
+        raise Bech32DecodeError('Witness programm too long')
+
+    if data[0] > 16:
+        raise Bech32DecodeError('Invalid witness version')
+
+    if data[0] == 0 and (len(decoded) not in (20, 32)):
+        raise Bech32DecodeError('Could not interpret witness programm')
+
+    return data[0], decoded
+
+
+def encode(hrp, witver, witprog):
+    """Encode a segwit address."""
+    return bech32_encode(hrp, [witver] + convertbits(witprog, 8, 5))
+
