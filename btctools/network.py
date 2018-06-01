eb88dacd16c2aa9b76ea88dd75c523386d44336d
Mike Dallas <mc-dallas@hotmail.com>
2018-06-01 23:47:48 +0100

    swap network bytes for xpub xprv

--- btctools/network.py
+++ btctools/network.py
@@ -15,8 +15,8 @@ main = {
     'keyhash': b'\x00',
     'scripthash': b'\x05',
     'wif': b'\x80',
-    'xprv': b'\x04\x88\xb2\x1e',
-    'xpub': b'\x04\x88\xad\xe4',
+    'xprv': b'\x04\x88\xad\xe4',
+    'xpub': b'\x04\x88\xb2\x1e',
     'utxo_url': 'https://blockchain.info/unspent?active={address}',
     'rawtx_url': 'https://blockchain.info/rawtx/{txid}?format=hex',
     'broadcast_url': 'https://blockchain.info/pushtx'
@@ -28,8 +28,8 @@ test = {
     'keyhash': b'\x6f',
     'scripthash': b'\xc4',
     'wif': b'\xef',
-    'xprv': b'\x04\x35\x87\xcf',
-    'xpub': b'\x04\x35\x83\x94',
+    'xprv': b'\x04\x35\x83\x94',
+    'xpub': b'\x04\x35\x87\xcf',
     'utxo_url': 'https://testnet.blockchain.info/unspent?active={address}',
     'rawtx_url': 'https://testnet.blockchain.info/rawtx/{txid}?format=hex',
     'broadcast_url': 'https://testnet.blockchain.info/pushtx'
