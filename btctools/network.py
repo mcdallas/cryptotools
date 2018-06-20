86d697a596616c9538348cc41405d695717022ec
mcdallas <mc-dallas@hotmail.com>
2018-06-20 11:49:46 +0100

    update testnet extended key version bytes

--- btctools/network.py
+++ btctools/network.py
@@ -17,6 +17,7 @@ main = {
     'scripthash': b'\x05',
     'wif': b'\x80',
     'extended_prv': {
+        # https://github.com/spesmilo/electrum-docs/blob/master/xpub_version_bytes.rst
         ADDRESS.P2PKH: b'\x04\x88\xad\xe4',  # xprv
         ADDRESS.P2WPKH: b'\x04\xb2\x43\x0c',  # zprv
         ADDRESS.P2WSH: b'\x02\xaa\x7a\x99',  # Zprv
@@ -24,6 +25,7 @@ main = {
         ADDRESS.P2WSH_P2SH: b'\x02\x95\xb4\x3f'  # Yprv
     },
     'extended_pub': {
+        # https://github.com/spesmilo/electrum-docs/blob/master/xpub_version_bytes.rst
         ADDRESS.P2PKH: b'\x04\x88\xb2\x1e',  # xpub
         ADDRESS.P2WPKH: b'\x04\xb2\x47\x46',  # zpub
         ADDRESS.P2WSH: b'\x02\xaa\x7e\xd3',  # Zpub
@@ -42,20 +44,20 @@ test = {
     'scripthash': b'\xc4',
     'wif': b'\xef',
     'extended_prv': {
-        ADDRESS.P2PKH: b'\x04\x35\x83\x94',
-        ADDRESS.P2SH: b'\x04\x35\x83\x94',
-        ADDRESS.P2WPKH: b'\x04\x5f\x18\xbc',
-        ADDRESS.P2WSH: b'\x02\x57\x50\x48',
-        ADDRESS.P2WPKH_P2SH: b'\x04\x4a\x4e\x28',
-        ADDRESS.P2WSH_P2SH: b'\x02\x42\x85\xb5'
+        # https://github.com/spesmilo/electrum-docs/blob/master/xpub_version_bytes.rst
+        ADDRESS.P2PKH: b'\x04\x35\x83\x94',  # tprv
+        ADDRESS.P2WPKH: b'\x04\x5f\x18\xbc',  # vprv
+        ADDRESS.P2WSH: b'\x02\x57\x50\x48',  # Vprv
+        ADDRESS.P2WPKH_P2SH: b'\x04\x4a\x4e\x28',  # uprv
+        ADDRESS.P2WSH_P2SH: b'\x02\x42\x85\xb5'  # Uprv
     },
     'extended_pub': {
-        ADDRESS.P2PKH: b'\x04\x35\x87\xcf',
-        ADDRESS.P2SH: b'\x04\x35\x87\xcf',
-        ADDRESS.P2WPKH: b'\x04\x5f\x1c\xf6',
-        ADDRESS.P2WSH: b'\x02\x57\x54\x83',
-        ADDRESS.P2WPKH_P2SH: b'\x04\x4a\x52\x62',
-        ADDRESS.P2WSH_P2SH: b'\x02\x42\x89\xef'
+        # https://github.com/spesmilo/electrum-docs/blob/master/xpub_version_bytes.rst
+        ADDRESS.P2PKH: b'\x04\x35\x87\xcf',  # tpub
+        ADDRESS.P2WPKH: b'\x04\x5f\x1c\xf6',  # vpub
+        ADDRESS.P2WSH: b'\x02\x57\x54\x83',  # Vpub
+        ADDRESS.P2WPKH_P2SH: b'\x04\x4a\x52\x62',  # upub
+        ADDRESS.P2WSH_P2SH: b'\x02\x42\x89\xef'  # Upub
     },
     'utxo_url': 'https://testnet.blockchain.info/unspent?active={address}',
     'rawtx_url': 'https://testnet.blockchain.info/rawtx/{txid}?format=hex',
